3ecc738a0c47f6581a058d0bac90a274d3a4cca0
Chris Elsmore <elsmorian@gmail.com>
2012-02-07 12:35:59 +0000

    Basic App

---
+++ helloplaces.app
@@ -0,0 +1,13 @@
+{
+    "title":"Hello Commutes",
+    "action":"View places",
+    "desc":"A simple way to view your journeys",
+    "status":"unstable",
+    "static":"true",
+    "uses":["twitter", "foursquare", "glatitude"],
+    "viewer":"places",
+    "author":"Chris Elsmore",
+    "github":"https://github.com/elsmorian/Locker",
+    "autoInstall":true,
+    "handle":"hellocommutes"
+}
---
+++ static/js/helloplaces.js
@@ -0,0 +1,40 @@
+/* Generic log function for debugging. */
+var log = function(msg) { if (console && console.log) console.debug(msg); }
+window.onload = loadFunct;
+
+function getPlaces(offset, limit, mysort, myterms) {
+    $.getJSON(
+        '/query/getPlace',
+        {'offset':offset, 'limit':limit, 'sort': mysort, 'terms':myterms},
+        gotPlaces
+    );
+}
+
+//http://localhost:8042/query/getPlace?terms=[me:true,%20network:'glatitude']&limit=10&offset=0&sort='{%22at:1%22}'
+
+
+function loadFunct() {
+    console.log("LoadFunct!");
+    //getPlaces(0, 10, '\'{\"at:1\"}\'', '[network:\'glatitude\',me:true]');
+    
+}
+
+function gotPlaces(places) {
+    for (var i in places) {
+        place = places[i];
+        place.at = new Date(place.at);
+    }
+    console.log(places);
+}
+
+//function loadScript() {
+//    var script = document.createElement('script');
+//    script.type = 'text/javascript';
+//    script.src = 'https://maps.googleapis.com/maps/api/js?sensor=false&callback=getLatestPlace';
+//    document.body.appendChild(script);
+//}
+
+
+$(function() {
+    console.log("DOM DONE!");
+});
