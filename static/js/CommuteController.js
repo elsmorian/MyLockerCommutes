b974512c7c0c5ff1f6fe1cd493adfda4dde1ad9f
Chris Elsmore <elsmorian@gmail.com>
2012-04-25 15:49:07 +0100

    initial get Data

--- static/js/CommuteController.js
+++ static/js/CommuteController.js
@@ -10,7 +10,9 @@ function gotPlaces(places) {
 }
 
 
-$(function() {
+//$(function() {
+$.noConflict();
+jQuery(document).ready(function($) {
     console.log("DOM DONE!");
     $("#startDatePicker").datepicker();
     $("#endDatePicker").datepicker();
@@ -28,22 +30,24 @@ function printState(state){
 
 function printDataToScreen(data){
     console.log(data);
-    $("#myData").html(data);
+    jQuery("#myData").html(data);
 }
 
 function getData(){
     var req = {}
-    var limitNum = $("#limitNum").val();
-    var offsetNum = $("#offsetNum").val();
+    var limitNum = jQuery("#limitNum").val();
+    var offsetNum = jQuery("#offsetNum").val();
     if (limitNum !== ''){
         req.limit = limitNum;
     }
     if (offsetNum !== ''){
         req.offset = offsetNum;
     }
-    var sDate = $("#startDatePicker").datepicker( "getDate" ).getTime();
-    var eDate = $("#endDatePicker").datepicker( "getDate" ).getTime();
+    var sDate = jQuery("#startDatePicker").datepicker( "getDate" ).getTime();
+    var eDate = jQuery("#endDatePicker").datepicker( "getDate" ).getTime();
     req.terms = "[me:true,at:1302440989000-]"
     console.log(req);
+    console.log(sDate);
+    console.log(edate);
     queryPlaces(req, printDataToScreen);
 }
